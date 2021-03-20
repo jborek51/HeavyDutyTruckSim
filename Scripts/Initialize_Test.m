062dd5fe451a5b880430636ce4186e0f85ebe049
John <jborek@uncc.edu>
2021-03-20 15:24:30 -0400

    modified the test scenario initialization script to be a 5 km straight route - JB

--- Scripts/Initialize_Test.m
+++ Scripts/Initialize_Test.m
@@ -2,9 +2,6 @@
 T_mode = 0;                                                 %   Traffic mode: 1 = on, 0 = off
 load('Max_Torque_Curve.mat')                                %   MPC max torque limit
 load('UNCC_Sim_Variables_R29_Half.mat')
-UNCC.Z_vec = zeros(length(UNCC.Z_vec),1);
-UNCC.theta_vec = zeros(length(UNCC.Z_vec),1);
-UNCC.V_lim = 20*ones(length(UNCC.Z_vec),1);
 load(strcat('Traffic_Data_R29_',num2str(T),'.mat'))         %   Load lead vehicle data
 load('R29_Intersection_Data.mat')
 load('stopping_profile.mat')
@@ -13,3 +10,8 @@ t_stop_adj = 999;                                           %   Baseline overrid
 sig_info(:,:,1) = Int.C;    
 sig_info(:,:,2) = Int.G;    
 sig_info(:,:,3) = Int.Cs;
+
+UNCC.X_vec = linspace(0,5000,1000);
+UNCC.Z_vec = zeros(length(UNCC.X_vec),1);
+UNCC.theta_vec = zeros(length(UNCC.X_vec),1);
+UNCC.V_lim = 20*ones(length(UNCC.X_vec),1);
